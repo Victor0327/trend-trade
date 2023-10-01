628db444ddf29b9585c0bacf9548696e64e23a8e
victor sheng <lsheng@flamingo.shop>
2023-10-01 15:52:47 +0800

    update

--- backend/server/controller/bar.py
+++ backend/server/controller/bar.py
@@ -1,6 +1,11 @@
 import numpy as np
 import yfinance as yf
 
+proxies = {
+   'http': 'http://127.0.0.1:7890',
+   'https': 'https://127.0.0.1:7890',
+}
+
 def row_to_dict(row):
   return {
       'open': row['Open'],
@@ -14,7 +19,7 @@ def row_to_dict(row):
 def get_bars(symbol, period="10d", interval="60m"):
   try:
     symbol_ticker = yf.Ticker(symbol)
-    symbol_bars = symbol_ticker.history(period=period, interval=interval)
+    symbol_bars = symbol_ticker.history(period=period, interval=interval, proxy=proxies)
 
     result = symbol_bars.apply(row_to_dict, axis=1).tolist()
   except:
